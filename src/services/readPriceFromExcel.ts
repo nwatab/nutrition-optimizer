dd14d42f31096c00db5bb0e9763d3008c4bc8c45
Naoki Watanabe <asterisk37n@gmail.com>
2025-03-28 00:08:35 +0900

    fix price reader and add more data

--- src/services/readPriceFromExcel.ts
+++ src/services/readPriceFromExcel.ts
@@ -1,8 +1,6 @@
-import { promises as fs } from 'fs';
 import * as xlsx from 'xlsx';
 
-export const readPriceDataFromExcel = async (filepath: string) => {
-  const buffer = await fs.readFile(filepath);
+export const readDataFromExcelBuffer = (buffer: Buffer) => {
   const workbook = xlsx.read(buffer, { type: 'buffer' });
   const sheetName = workbook.SheetNames[0]; // Assuming first sheet
   const sheet = workbook.Sheets[sheetName];
@@ -13,27 +11,51 @@ export const readPriceDataFromExcel = async (filepath: string) => {
   return data;
 };
 
-export const getPriceFromExcelData =
+export const trimData = (table: (string | number)[][]) => {
+  return table
+    .filter((row) => row.some((cell) => cell !== ''))
+    .map((row) =>
+      row.slice(
+        // 最初に全て0でないカラムのindexを取得。見つからなければ全カラム数を返す
+        table[0].findIndex(
+          (_, colIndex) => !table.every((row) => row[colIndex] === '')
+        ) === -1
+          ? row.length
+          : table[0].findIndex(
+              (_, colIndex) => !table.every((row) => row[colIndex] === 0)
+            )
+      )
+    );
+};
+
+// itemCodeに関する情報を取り出す関数と、その出力結果から価格を抽出する関数を分けた方が良い
+export const makeReadPriceFromExcelData =
   (data: (string | number)[][]) =>
   (itemCode: number): number | null => {
     // Locate item codes (Row 10, index 9)
-    const ITEM_CODE_ROW = 9;
+    const ITEM_CODE_ROW = 7;
     const itemCodes = data[ITEM_CODE_ROW];
+
     const columnIndex = itemCodes.findIndex((code) => code === itemCode);
     if (columnIndex === -1) {
       return null;
     }
 
-    // Retrieve unit (Row 12, index 11)
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const unit = data[11][columnIndex] ?? '';
-
-    // Retrieve price (Row 27, index 26)
-    const price = parseFloat(data[26][columnIndex] as string);
-    if (isNaN(price)) {
-      console.error('Invalid price value');
-      return null;
-    }
-
-    return price;
+    const recentPrices = data
+      .map(
+        (row) =>
+          ({ monthCode: row[1], season: row[4], price: row[columnIndex] }) as {
+            monthCode: string;
+            season: string;
+            price: number;
+          }
+      )
+      .filter(
+        (data) => data.monthCode.length > 0 && typeof data.price === 'number'
+      )
+      .slice(-3);
+    const averagePrice =
+      recentPrices.reduce((acc, data) => acc + data.price, 0) /
+      recentPrices.length;
+    return averagePrice;
   };
